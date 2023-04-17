303d3fbef80ea81ca46952f33fbda46df95f132c
aclbrandao <93746110+aclbrandao@users.noreply.github.com>
2023-04-16 21:12:25 -0300

    Delete iDaoModeCrud.php

--- iDaoModeCrud.php
+++
@@ -1,7 +0,0 @@
-<?php 
-interface iDaoModeCrud {
-              public function create( $nome, $telefone, $email );
-              public function read( $id );
-              public function update( $id, $nome, $telefone, $emailt );
-              public function delete( $param );
-          }
